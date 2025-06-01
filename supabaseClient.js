3e3641f6c6745216a63c785c77babd0ec605c910
vedant bagal <156566344+VedantBagal@users.noreply.github.com>
2025-06-01 16:17:35 +0530

    Update supabaseClient.js
    supa key exposed

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 // supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'; // Using ES Module import
 
-const supabaseUrl = 'https://dqvltutvyytoaqwikwxy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxdmx0dXR2eXl0b2Fxd2lrd3h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNTQ5OTYsImV4cCI6MjA2MzczMDk5Nn0.vPT83mAeO2IDQSwfCZPUP5aFBpKXTQeArSpbfxzPhW0';
+const supabaseUrl = 'https://vltutvyytoaqwikwxy.supabase.co';
+const supabaseKey = 'JhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxdmx0dXR2eXl0b2Fxd2lrd3h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNTQ5OTYsImV4cCI6MjA2MzczMDk5Nn0.vPT83mAeO2IDQSwfCZPUP5aFBpKXTQeArSpbfxzPhW0';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
